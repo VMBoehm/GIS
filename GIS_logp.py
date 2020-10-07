7884d2645fcdad6e3019e99519db78fe53045caa
Vanessa Boehm <vboehm@berkeley.edu>
2020-10-07 12:30:50 -0700

    rewritten GIS

--- GIS_logp.py
+++ GIS_logp.py
@@ -1,6 +1,9 @@
 from SIT import *
 from load_data import * 
 import argparse
+import os
+import pickle
+from sklearn.decomposition import PCA
 
 parser = argparse.ArgumentParser()
 
@@ -36,7 +39,32 @@ class Data():
         else:
             self.type = 'flat'
 
-            data_train, data_val, data_test = globals()["load_data_%s"%params['dataset']]()    
+            data_train, data_val, data_test = globals()["load_data_%s"%params['dataset']]() 
+
+        if self.type =='image':
+
+            data_val   = data_train[-10000:]
+            data_train = data_train[:-10000]
+    
+            data_train = (data_train+np.random.rand(np.prod(data_train.shape)).reshape(data_train.shape))/256.
+            data_val   = (data_val+np.random.rand(np.prod(data_val.shape)).reshape(data_val.shape))/256.
+            data_test  = (data_test+np.random.rand(np.prod(data_test)).reshape(data_test.shape))/256.
+
+        if params['pca']:
+            pca = PCA()
+            pca.fit(data_train)
+            assert(params['pca_cut']<0.2)
+            print(pca.explained_variance_ratio_)
+            num = np.where(pca.explained_variance_ratio_>params['pca_cut'])[0][-1]
+            print('using ', num, ' components')
+            pca = PCA(n_components=num)
+            pca.fit(data_train)
+            
+            data_train = pca.transform(data_train)
+            data_val   = pca.transform(data_val)
+            data_test  = pca.transform(data_test)
+
+            self.type  = 'flat'
         
         if self.type=='flat':
             self.train = torch.tensor(data_train).float().to(device)
@@ -44,12 +72,10 @@ class Data():
             self.test  = torch.tensor(data_test).float().to(device)
         else:
             self.train = torch.tensor(data_train).float().reshape(len(data_train), -1).to(device)
-            self.train = (self.train + torch.rand_like(self.train)) / 256.
             self.test  = torch.tensor(data_test).float().reshape(len(data_test), -1).to(device)
-            self.test  = (self.test + torch.rand_like(self.test)) / 256.
+            self.val   = torch.tensor(data_val).float().reshape(len(data_val), -1).to(device)
     
-            self.val   = self.train[-10000:]
-            self.train = self.train[:-10000]
+
     
         if params['train_size'] > 0:
             assert params['train_size'] <= len(self.train)
@@ -119,9 +145,10 @@ class GIS():
             self.best_Nlayer = 1 
 
         wait = 0
+        i = len(self.model.layer) 
         #sliced transport layer
-        while True:
-
+        while i<20:
+            print('i %d \n'%d)
             t = time.time()
             i = len(self.model.layer)
             if self.data.type=='flat':
@@ -146,7 +173,6 @@ class GIS():
             SWD1 = layer.fit_spline(data=self.data.train, edge_bins=params['edge_bins'], derivclip=params['derivclip'], alpha=params['alpha'], noise_threshold=params['noise_threshold'], KDE=params['KDE'], bw_factor=params['bw_factor_data'], batchsize=params['batchsize'], verbose=params['verbose'])
             batchsize = params['batchsize']
             if (SWD1>params['noise_threshold']).any():
-
                 j = 0
                 while j * batchsize < len(self.data.train):
                     self.data.train[j*batchsize:(j+1)*batchsize], logj_train1 = layer(self.data.train[j*batchsize:(j+1)*batchsize])
@@ -184,16 +210,26 @@ class GIS():
 
         self.model.layer = self.model.layer[:best_Nlayer]
         print ('best logp:', self.logp_train[best_Nlayer-1], self.logp_validate[best_Nlayer-1], self.logp_test[best_Nlayer-1], 'time:', time.time()-t_total, 'iteration:', len(self.model.layer))
-    
+
     def save_model(self):
         torch.save(model, params('model_file'))
 
-    def evaluate(self,x):
-        pass
+    def fwd(self,x):
+        return self.model.forward(x)
+
+    def logp(self,x):
+        return model.evaluate_density(x)
 
 if __name__=='__main__':
 
     ROOT = '/global/scratch/vboehm/gis/'
+    LOC  = './params/'
+
+    if not os.path.isdir(ROOT):
+        os.makedirs(ROOT)
+
+    if not os.path.isdir(LOC):
+        os.makedirs(LOC)
 
     args = parser.parse_args()
 
@@ -203,25 +239,36 @@ if __name__=='__main__':
     assert torch.cuda.is_available()
     device = torch.device('cuda')
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    
+
+
+    params['pca']         = True
+    params['pca_cut']     = 0.001
+ 
     data = Data(params,device)
-    params['load'] = False
+
+    params['run_name']    = 'test'
+    params['load']        = False
     params['interp_nbin'] = min(200, int(len(data.train)**0.5)) 
-    params['KDE'] = True 
+    params['KDE']         = True 
     params['bw_factor_data'] = 1 
-    params['alpha'] = (0.9, 0.99) 
-    params['edge_bins'] = min(int(len(data.train)/200./params['interp_nbin']), 5) 
-    params['batchsize'] = 2**15 
-    params['derivclip'] = None 
+    params['alpha']       = (0.9, 0.99) 
+    params['edge_bins']   = min(int(len(data.train)/200./params['interp_nbin']), 5) 
+    params['batchsize']   = 2**15 
+    params['derivclip']   = None 
     params['noise_threshold'] = 0
-    params['verbose'] = True
-    params['ndata_wT'] = min(len(data.train), int(math.log(data.ndim)*50000)) 
-    params['ndata_spline'] = len(data.train) 
+    params['verbose']     = True
+    params['ndata_wT']    = min(len(data.train), int(math.log(data.ndim)*50000)) 
+    params['ndata_spline']  = len(data.train) 
     params['MSWD_max_iter'] = min(len(data.train) // data.ndim, 200)
-    params['root'] = ROOT
-    PATH = '%d/'
-    params['model_file'] = os.path.join(
+    params['savio_root']    = ROOT
+    params['model_file']    = os.path.join(ROOT, '%s'%params['run_name'])
+    params['params_file']   = os.path.join(LOC,'%s.pkl'%params['run_name'])
+
+    pickle.dump(params,open(params['params_file'],'wb'))
 
     gis = GIS(params, data)
     gis.set_rand()
     gis.train()
+    gis.save()
+    print(gis.fwd(data.train[0:1]))
+    print(torch.mean(gis.logp(data.train)))
